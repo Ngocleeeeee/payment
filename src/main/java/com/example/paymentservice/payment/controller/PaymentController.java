a6a7001924c355e24430cb675a613b152a05c2fb
Ngoc <zaihole2003@gmail.com>
2024-10-04 13:50:34 +0700

    .

---
+++ src/main/java/com/example/paymentservice/payment/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.example.paymentservice.payment.controller;
+
+import com.example.paymentservice.payment.service.VNPAYService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "payment")
+public class PaymentController {
+    @Autowired
+    private VNPAYService vnPayService;
+
+    @PostMapping("/submitOrder")
+    public Map<String, String> submitOrder(@RequestParam("amount") int orderTotal,
+                                           @RequestParam("orderInfo") String orderInfo,
+                                           HttpServletRequest request) {
+        return vnPayService.submitOrder(request, orderTotal, orderInfo);
+    }
+
+    @GetMapping("/vnpay-payment-return")
+    public Map<String, Object> paymentCompleted(HttpServletRequest request) {
+        return vnPayService.paymentCompleted(request);
+    }
+}
